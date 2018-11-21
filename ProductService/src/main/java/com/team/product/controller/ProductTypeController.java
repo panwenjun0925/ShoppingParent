ecfc1e0fa06dab29a67017d43e8bbaa0c22d8589
0725youqi <1399211854@qq.com>
2018-11-22 01:49:23 +0800

    添加没有成功的测试编写

--- ProductService/src/main/java/com/team/product/controller/ProductTypeController.java
+++ ProductService/src/main/java/com/team/product/controller/ProductTypeController.java
@@ -15,4 +15,6 @@ public class ProductTypeController {
 
     @Autowired
     private ProductTypeService productTypeService;
+
+
 }
--- ProductService/src/main/java/com/team/product/service/Impl/ProductTypeServiceImpl.java
+++ ProductService/src/main/java/com/team/product/service/Impl/ProductTypeServiceImpl.java
@@ -1,11 +1,14 @@
 package com.team.product.service.Impl;
 
 
+import com.team.facade.pojo.GoodsType;
 import com.team.product.mapper.GoodsTypeMapper;
 import com.team.product.service.ProductTypeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Auther: YouQi
@@ -20,6 +23,33 @@ public class ProductTypeServiceImpl implements ProductTypeService {
     private GoodsTypeMapper goodsTypeMapper;
 
 
+    @Override
+    public List<GoodsType> queryList() {
+        return goodsTypeMapper.queryList();
+    }
 
+    @Override
+    public GoodsType queryByPrimaryKey(Integer Id) {
+        return goodsTypeMapper.queryByPrimaryKey(Id);
+    }
 
+    @Override
+    public List<GoodsType> queryGoodsTypeByExample(GoodsType goods) {
+        return goodsTypeMapper.queryGoodsTypeByExample(goods);
+    }
+
+    @Override
+    public Integer insertSelective(GoodsType goods) {
+        return goodsTypeMapper.insertSelective(goods);
+    }
+
+    @Override
+    public Integer deleteByPrimaryKey(Integer id) {
+        return goodsTypeMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public Integer updateByPrimaryKeySelective(GoodsType goods) {
+        return goodsTypeMapper.updateByPrimaryKeySelective(goods);
+    }
 }
