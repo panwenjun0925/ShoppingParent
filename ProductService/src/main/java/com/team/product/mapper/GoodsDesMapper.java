721ec81ce8a09c420619e7a02e89e6f43323a0ce
0725youqi <1399211854@qq.com>
2018-11-21 15:28:16 +0800

    更新了属性

--- ProductService/src/main/java/com/team/product/mapper/GoodsDesMapper.java
+++ ProductService/src/main/java/com/team/product/mapper/GoodsDesMapper.java
@@ -1,6 +1,6 @@
 package com.team.product.mapper;
 
-import com.team.facade.pojo.Goods;
+import com.team.facade.pojo.GoodsDes;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -8,53 +8,44 @@ import java.util.List;
 
 /**
  * @Auther: YouQi
- * @Date: 2018/11/21 10:30
+ * @Date: 2018/11/21 13:35
  * @Description:
  * @Version: 1.0
  */
 @Repository
 @Mapper
-public interface GoodsMapper {
-    /**
-     * 功能描述:查找所有商品 建议搭配分页插件
-     *
-     * @param: []
-     * @return: java.util.List<com.team.facade.pojo.Goods>
-     * @auther: YouQi
-     * @date: 2018/11/21 10:44
-     **/
-    List<Goods> queryList();
+public interface GoodsDesMapper {
+   /**
+    * 功能描述:查找所有商品详情
+    *
+    * @param: []
+    * @return: java.util.List<com.team.facade.pojo.GoodsDes>
+    * @auther: YouQi
+    * @date: 2018/11/21 14:50
+    **/
+    List<GoodsDes> queryList();
 
     /**
      * 功能描述:根据主键查找
      *
      * @param: [Id]
-     * @return: com.team.facade.pojo.Goods
+     * @return: com.team.facade.pojo.GoodsDes
      * @auther: YouQi
      * @date: 2018/11/21 10:53
      **/
-    Goods queryByPrimaryKey(Integer Id);
+    GoodsDes queryByPrimaryKey(Integer Id);
 
-
-    /**
-     * 功能描述:查找小于此价格的商品 最好降序排列
-     *
-     * @param: [price]
-     * @return: java.util.List<com.team.facade.pojo.Goods>
-     * @auther: YouQi
-     * @date: 2018/11/21 11:00
-     **/
-    List<Goods> queryByRealPrice(Double price);
 
     /**
      * 功能描述:根据传入的对象搜索 为null属性不成立
      *
      * @param: [goods]
-     * @return: java.util.List<com.team.facade.pojo.Goods>
+     * @return: java.util.List<com.team.facade.pojo.GoodsDes>
      * @auther: YouQi
      * @date: 2018/11/21 10:49
      **/
-    List<Goods> queryGoodsByExample(Goods goods);
+    List<GoodsDes> queryGoodsDesByExample(GoodsDes goods);
+
 
     /**
      * 功能描述:返回受影响的行数 属性为null不成立
@@ -64,7 +55,7 @@ public interface GoodsMapper {
      * @auther: YouQi
      * @date: 2018/11/21 10:52
      **/
-    Integer insertSelective(Goods goods);
+    Integer insertSelective(GoodsDes goods);
 
     /**
      * 功能描述:根据主键删除
@@ -84,15 +75,7 @@ public interface GoodsMapper {
      * @auther: YouQi
      * @date: 2018/11/21 10:56
      **/
-    Integer updateByPrimaryKeySelective(Goods goods);
+    Integer updateByPrimaryKeySelective(GoodsDes goods);
 
-    /**
-     * 功能描述:查询所有的已下架商品
-     *
-     * @param: []
-     * @return: java.util.List<com.team.facade.pojo.Goods>
-     * @auther: YouQi
-     * @date: 2018/11/21 14:01
-     **/
-    List<Goods> queryByIsPutaway();
+
 }
