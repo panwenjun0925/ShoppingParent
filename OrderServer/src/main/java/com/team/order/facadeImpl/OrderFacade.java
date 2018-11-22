cf16905feca23d1d11a37d14e7a006eab1d3730d
panwenjun0925 <1664222510@qq.com>
2018-11-22 18:05:02 +0800

    完成了dbunit的测试和服务的修改

--- OrderServer/src/main/java/com/team/order/facadeImpl/OrderFacade.java
+++ OrderServer/src/main/java/com/team/order/facadeImpl/OrderFacade.java
@@ -1,8 +1,8 @@
-package com.team.order.service.Impl;
+package com.team.order.facadeImpl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.team.facade.IFacade.IOrderFacade;
 import com.team.facade.pojo.Order;
-import com.team.order.mapper.OrderMapper;
 import com.team.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,33 +11,33 @@ import java.util.List;
 
 /**
  * @Auther: PWJ
- * @Date: 2018/11/20 20:24
- * @Description:订单的service实现类
+ * @Date: 2018/11/22 12:10
+ * @Description:中间服务的实现
  * @Version: 1.0
  */
 @Service
 @Component
-public class OrderServiceImpl implements OrderService {
-
+public class OrderFacade implements IOrderFacade {
     @Autowired
-    OrderMapper mapper;
+    OrderService service;
+
     @Override
     public List<Order> getOrderByCondition(Order order) {
-        return mapper.getOrderByCondition(order);
+        return service.getOrderByCondition(order);
     }
 
     @Override
     public int addOrder(Order order) {
-        return mapper.addOrder(order);
+        return service.addOrder(order);
     }
 
     @Override
     public int delOrderByOrderId(int id) {
-        return mapper.delOrderByOrderId(id);
+        return service.delOrderByOrderId(id);
     }
 
     @Override
     public int updateOrder(Order order) {
-        return mapper.updateOrder(order);
+        return service.updateOrder(order);
     }
 }
