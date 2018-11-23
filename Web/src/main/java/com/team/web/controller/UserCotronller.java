68e952eb62a7c1210db7a15e865dd1d0717f0538
qinbencheng <q1256531958@163.com>
2018-11-23 11:23:40 +0800

    添加了address，user的controller

---
+++ Web/src/main/java/com/team/web/controller/UserCotronller.java
@@ -0,0 +1,61 @@
+package com.team.web.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.team.facade.IFacade.IUserFacade;
+import com.team.facade.pojo.User;
+import com.team.facade.vo.userVo.UserVo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @Auther: qin
+ * @Date: 2018/11/23 10:26
+ * @Description:
+ * @Version: 1.0
+ */
+@Controller
+public class UserCotronller {
+    @Reference
+    private IUserFacade facade;
+
+    @PostMapping("/login")
+    @ResponseBody
+    public String login(UserVo userVo){
+        List<User> all = facade.getAll(userVo);
+        if(null!=all.get(0)){
+            return "登陆成功";
+        }else {
+            return "没有你";
+        }
+    }
+
+    @RequestMapping("/getAll")
+    public String getALl(UserVo userVo, Model model){
+        List<User> all = facade.getAll(userVo);
+        model.addAttribute("users",all);
+        return "";
+    }
+    @PostMapping("/addUser")
+    public String addUser(User user){
+        facade.add(user);
+        return "";
+    }
+
+    @PostMapping("updateUser")
+    public String updateUser(User user){
+        facade.update(user);
+        return "";
+    }
+
+    @PostMapping("/delete")
+    public String deleteById(User user){
+        facade.deleteById(user);
+        return "";
+    }
+
+}
